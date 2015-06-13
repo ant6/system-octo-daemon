7fedd70dcb180e6a6a5e858f16e23997da739443
ant6 <ant6ant@gmail.com>
2015-06-13 19:46:04 +0200

    added script removing packages

---
+++ tools/antainer_remove_package.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# this script removes software from given container
+
+print_ok='[\e[1;32m  OK  \e[0m] '
+print_err='[\e[1;31mERROR\e[0m] '
+
+# check for root privileges
+if [ "$(whoami)" != "root" ]; then
+	echo -e $print_err "Sorry, you are not root."
+	exit 1
+fi
+
+# get container location
+if [ -d $1 ] && [ "$1" != "" ]
+then
+	container_location=$1
+	echo -e $print_ok "Container location set to: " $1
+	shift
+else
+	echo -e $print_err "Wrong container location!"
+	echo "use: $0 <container location> <package1 [package2 ...]>"
+	exit 37
+fi
+
+# get packages to install
+while [ $# -gt 0 ]
+do
+    packages="$packages $1"
+    shift
+done
+
+yum -y --nogpg --installroot=$container_location remove $packages
+
+if [ $(echo $?) != 0 ]
+then
+	echo -e $print_err "Something went horribly wrong :|"
+	exit 137
+else
+	echo -e $print_ok "Done removing:" $packages
+fi
